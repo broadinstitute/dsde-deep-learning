b5a5ad7c5982e24b841575478f7f2b755279c087
Samuel Friedman <sam@broadinstitute.org>
2017-02-23 10:52:46 -0500

    added dotkits for using gpu on gsa

---
+++ api_tutorials/dotkits.sh
@@ -0,0 +1,8 @@
+use Anaconda
+use .cuda-7.0-ea
+use .gcc-4.9.1
+use .openblas-0.2.8
+
+export LD_LIBRARY_PATH=/dsde/working/sam/cnn/cuda/lib64/:$LD_LIBRARY_PATH
+export CPATH=/dsde/working/sam/cnn/cuda/include/:$CPATH
+export LIBRARY_PATH=/dsde/working/sam/cnn/cuda/lib64:$LIBRARY_PATH
