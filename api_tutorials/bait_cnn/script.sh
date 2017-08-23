3da4c6c26700041dd7eae31f7a23f5c4dce0d9de
Yossi Farjoun <farjoun@gmail.com>
2017-08-23 12:01:50 +0300

    added perfomance plot. network not really working, but learning a little...

--- api_tutorials/bait_cnn/script.sh
+++ api_tutorials/bait_cnn/script.sh
@@ -6,4 +6,7 @@ bedtools coverage -counts -a whole_exome_illumina_coding_v1.Homo_sapiens_assembl
 bedtools intersect  -a whole_exome_illumina_coding_v1.Homo_sapiens_assembly19.baits.bed -b singleton.targets.bed > singleton.baits.bed
 
 tail -n+2 NexPond-647561.per_target_coverage | awk 'BEGIN{FS="\t";OFS="\t"}{print $1, $2-1,$3,$6,$14}' > coverage.bed
+tail -n+2 NexPond-647561.coverage | awk 'BEGIN{FS="\t";OFS="\t"}{print $1, $2-1,$3,$6,$7}' > coverage2.bed
 bedtools intersect -a coverage.bed -b singleton.baits.bed > coverage.singleton.bait.bed
+bedtools intersect -a coverage2.bed -b singleton.baits.bed > temp
+bedtools sort -i temp > coverage2.singleton.bait.bed 
