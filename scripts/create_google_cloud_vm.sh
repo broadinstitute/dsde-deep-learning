90aa5c072cd54f49bcffc206d168dda08d93e23f
Samuel Friedman <sam@broadinstitute.org>
2018-06-14 08:51:12 -0400

    scr

--- scripts/create_google_cloud_vm.sh
+++ scripts/create_google_cloud_vm.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-ECHO=echo
+#
+# This script creates a GPU enabled Google cloud VM instance
+# It is meant to be run on a local or GSA server machine
+# After running this run the companion script setup_google_cloud_vm.sh on the instance
+#
+# June 2018
+# Sam Friedman 
+# sam@broadinstitute.org
 
+ECHO=echo
+ZONE=us-east1-d
 VM_NAME="dsde-deep-learner-$USER"
+BOOT_DISK_GB=200
+
+# Create the VM with gcloud
 $ECHO echo "Try to create vm named ${VM_NAME}"
 $ECHO gcloud compute instances create "${VM_NAME}" \
 	--scopes "compute-rw,storage-full,cloud-platform" \
 	--image-family "ubuntu-1604-lts" --image-project "ubuntu-os-cloud" \
 	--machine-type "n1-standard-4" \
 	--accelerator "type=nvidia-tesla-k80,count=1" \
-	--boot-disk-size "100" --boot-disk-type "pd-ssd" \
-	--zone "us-east1-d" \
+	--boot-disk-size "${BOOT_DISK_GB}" --boot-disk-type "pd-ssd" \
+	--zone "${ZONE}" \
 	--maintenance-policy TERMINATE --restart-on-failure
+
+# We also need NVidia's CuDNN libraries
+$ECHO "echo Signup as a NVIdia's developer and download the CuDNN libs from: https://developer.nvidia.com/cudnn"
+$ECHO "echo Or if you have Broad filesystem acces copy them from GSA with gcloud compute scp:"
+$ECHO gcloud compute scp --recurse /dsde/working/sam/scripts/cuda ${VM_NAME}:~ --zone $ZONE
 
 # Now try to SSH into the instance
 $ECHO gcloud compute ssh "${VM_NAME}" --zone us-east1-d
@@ -21,8 +38,5 @@ $ECHO "echo Then run the setup script with: "
 $ECHO "echo ./dsde-deep-learning/scripts/setup_google_cloud_vm.sh "
 
 $ECHO "echo To copy things from the VM to a local machine use gcloud compute scp "
-$ECHO "echo For example the templates templates learned and plotted by keras_example.py can be copied locally with:"
-$ECHO gcloud compute scp ${VM_NAME}:~/dsde-deep-learning/api_tutorials/frames/mnist/regression/templates_* ./ --zone us-east1-d
-
-
-
+$ECHO "echo For example the templates learned and plotted by keras_example.py can be copied locally with:"
+$ECHO gcloud compute scp --recurse ${VM_NAME}:~/regression_example ./ --zone $ZONE
--- scripts/setup_google_cloud_vm.sh
+++ scripts/setup_google_cloud_vm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# This script prepares a GPU enabled Google cloud VM instance for deep learning
+# It is meant to be run on the VM itself.  
+# SSH into the VM with: gcloud compute ssh "${VM_NAME}" --zone us-east1-d
+# This script is to be run after running the companion script create_google_cloud_vm.sh on a local machine
+#
+# June 2018
+# Sam Friedman 
+# sam@broadinstitute.org
+
 ECHO=echo
 
 # Get root
@@ -8,14 +18,25 @@ $ECHO sudo passwd
 if ! dpkg-query -W cuda-9-0; then
   # The 16.04 installer works with 16.10.
   $ECHO curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-  $ECHO dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-  $ECHO apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-  $ECHO apt-get update
-  $ECHO apt-get install cuda-9-0 -y
+  $ECHO sudo dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  $ECHO sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  $ECHO sudo apt-get update
+  $ECHO sudo apt-get install cuda-9-0 -y
 fi
 
+$ECHO source ~/.profile
+
 # Enable persistence mode
-$ECHO nvidia-smi -pm 1
+$ECHO sudo nvidia-smi -pm 1
+
+# Now we need to get NVidia's CuDNN libraries copied over and installed
+$ECHO "echo Signup as a NVIdia's developer and download the CuDNN libs from: https://developer.nvidia.com/cudnn"
+$ECHO "echo Or copy them from GSA or your local machine with gcloud compute scp:"
+$ECHO "echo gcloud compute scp --recurse /dsde/working/sam/scripts/cuda ${VM_NAME}:~ --zone us-east1-d"
+$ECHO sudo cp cuda/include/* /usr/local/cuda/include/
+$ECHO sudo cp cuda/lib64/* /usr/local/cuda/lib64/
+$ECHO sudo chmod 644 /usr/local/cuda/include/cudnn.h
+$ECHO sudo ldconfig /usr/local/cuda/lib64
 
 # Get Anaconda
 $ECHO wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
