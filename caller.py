feab831cb8c3c6895e9672dda0095eaa0ecc20c8
Samuel Friedman <sam@broadinstitute.org>
2018-07-27 15:57:15 -0400

    call fix5

--- caller.py
+++ caller.py
@@ -108,8 +108,8 @@ def infer_vcf(args):
 
 					if stats['batches_processed'] % 100 == 0:
 						elapsed = time.time() - start_time
-						v_per_minute = stats['batches processed']*args.batch_size / (elapsed/60)
-						print('At genomic position:', cur_pos, 'Tensors per minute:', ,'Batches processed:', stats['batches_processed'])
+						t_per_minute = stats['batches processed']*args.batch_size / (elapsed/60)
+						print('At genomic position:', k, cur_pos, 'Tensors per minute:', t_per_minute,'Batches processed:', stats['batches_processed'])
 						for s in stats.keys():
 							print(s, 'has:', stats[s])	
 	
