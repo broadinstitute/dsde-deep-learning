8c8d6bc32373cb22f30de2fc0be179ae38a6b601
Samuel Friedman <sam@broadinstitute.org>
2018-09-06 22:38:00 -0400

    gan

--- api_tutorials/gan.py
+++ api_tutorials/gan.py
@@ -359,7 +359,7 @@ def total_variation_norm(x):
 
 def build_imagenet_generative_model(args):
 	# Build Generative model ...
-	nch = 32
+	nch = 48
 	dense_channels = 16
 	inner_dim = 16
 	channel_axis = -1
@@ -377,11 +377,11 @@ def build_imagenet_generative_model(args):
 	H = batch_normalize_or_not(args, H, channel_axis)
 	H = Activation('relu')(H)
 	H = UpSampling2D(size=(2, 2))(H)
-	H = Conv2D(nch*2, (3, 3), padding='same', kernel_initializer='glorot_uniform')(H)
+	H = Conv2D(nch*4, (3, 3), padding='same', kernel_initializer='glorot_uniform')(H)
 	H = batch_normalize_or_not(args, H, channel_axis)
 	H = Activation('relu')(H)
 	H = UpSampling2D(size=(2, 2))(H)
-	H = Conv2D(nch*2, (3, 3), padding='same', kernel_initializer='glorot_uniform')(H)
+	H = Conv2D(nch*8, (3, 3), padding='same', kernel_initializer='glorot_uniform')(H)
 	H = Activation('relu')(H)	
 	pre_logit = Conv2D(3, (1, 1), padding='same', kernel_initializer='glorot_uniform')(H)
 	generation = Activation('sigmoid')(pre_logit)
