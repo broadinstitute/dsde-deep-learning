19b2322c4e9549c4ca9ecaa710fbc96dcb30cf36
Samuel Friedman <sam@broadinstitute.org>
2018-10-08 13:26:03 -0400

    hyperopts

--- hyperparameter_optimizer.py
+++ hyperparameter_optimizer.py
@@ -254,7 +254,7 @@ class HyperparameterOptimizer(object):
 					image_path = image_name if args.image_dir is None else args.image_dir + image_name
 					models.inspect_model(args, model, generate_train, generate_valid, image_path=image_path)
 				
-				limit_mem()
+				#limit_mem()
 				return loss_and_metrics[0]
 			
 			except ValueError as e:
