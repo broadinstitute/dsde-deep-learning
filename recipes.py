6d35e7b75a2d96684ba4a73d91f4905adebd2f47
Samuel Friedman <sam@broadinstitute.org>
2019-03-28 12:10:12 -0400

    res model updates

--- recipes.py
+++ recipes.py
@@ -1021,8 +1021,8 @@ def train_ref_read_residual(args):
 
 	_, _, generate_test = td.train_valid_test_generators_from_args(args, with_positions=True)
 	test = td.big_batch_from_minibatch_generator(args, generate_test)
-	plots.plot_roc_per_class(model, test_data, test[1], args.labels, args.id)
-	return plots.get_per_class_auc(model, test_data, test[1], args.labels)
+	plots.plot_roc_per_class(model, test[0], test[1], args.labels, args.id)
+	return plots.get_per_class_auc(model, test[0], test[1], args.labels)
 
 
 
